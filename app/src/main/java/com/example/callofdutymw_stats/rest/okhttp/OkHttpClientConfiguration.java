551598c2ebda0da38674f0d6337670da61790294
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-08-27 14:05:21 -0300

    - Client method configured;

---
+++ app/src/main/java/com/example/callofdutymw_stats/rest/okhttp/OkHttpClientConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.callofdutymw_stats.rest.okhttp;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkHttpClientConfiguration {
+
+    private OkHttpClientConfiguration() {
+    }
+
+    private static final String API_HOST = "call-of-duty-modern-warfare.p.rapidapi.com";
+    private static final String API_KEY = "xx-xx";
+
+    public static void configureHttpClientHeader(OkHttpClient.Builder okHttpClient) {
+        okHttpClient.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(@NotNull Interceptor.Chain chain) throws IOException {
+                Request original = chain.request();
+                Request request = original.newBuilder()
+                        .header("x-rapidapi-host", API_HOST)
+                        .header("x-rapidapi-key", API_KEY)
+                        .method(original.method(), original.body())
+                        .build();
+
+                return chain.proceed(request);
+            }
+        });
+    }
+}
