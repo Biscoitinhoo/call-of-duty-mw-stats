fb61f2a672adf3d1c806bd2e3b3933d08f7866bf
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-08-27 15:54:39 -0300

    - APIKey added again;

--- app/src/main/java/com/example/callofdutymw_stats/rest/okhttp/OkHttpClientConfiguration.java
+++ app/src/main/java/com/example/callofdutymw_stats/rest/okhttp/OkHttpClientConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.callofdutymw_stats.rest.okhttp;
 
+import com.example.callofdutymw_stats.BuildConfig;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -15,7 +17,7 @@ public class OkHttpClientConfiguration {
     }
 
     private static final String API_HOST = "call-of-duty-modern-warfare.p.rapidapi.com";
-    private static final String API_KEY = "xx-xx";
+    private static final String API_KEY = "2ae71c798dmshe5e282e77dcbf38p1b359cjsndc5b1e715bb7";
 
     public static void configureHttpClientHeader(OkHttpClient.Builder okHttpClient) {
         okHttpClient.addInterceptor(new Interceptor() {
