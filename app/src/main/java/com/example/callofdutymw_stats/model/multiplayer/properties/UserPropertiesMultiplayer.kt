a79a26c94dfc78de1b924753a3dbcce421932f01
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-08-28 08:44:06 -0300

    - UserPropertiesMultiplayer.kt with all params;

--- app/src/main/java/com/example/callofdutymw_stats/model/multiplayer/properties/UserPropertiesMultiplayer.kt
+++ app/src/main/java/com/example/callofdutymw_stats/model/multiplayer/properties/UserPropertiesMultiplayer.kt
@@ -1,5 +1,42 @@
 package com.example.callofdutymw_stats.model.multiplayer.properties
 
-class UserPropertiesMultiplayer {
+import com.google.gson.annotations.SerializedName
 
-}
+class UserPropertiesMultiplayer(
+    @SerializedName("recordLongestWinStreak")
+    val recordLongestWinStreak: String,
+    @SerializedName("recordXpInAMatch")
+    val recordXP: String,
+    @SerializedName("accuracy")
+    val accuracy: String,
+    @SerializedName("losses")
+    val losses: String,
+    @SerializedName("totalGamesPlayed")
+    val totalGamesPlayed: String,
+    @SerializedName("score")
+    val score: String,
+    @SerializedName("deaths")
+    val deaths: String,
+    @SerializedName("wins")
+    val wins: String,
+    @SerializedName("kdRatio")
+    val kdRatio: String,
+    @SerializedName("bestAssists")
+    val bestAssists: String,
+    @SerializedName("bestScore")
+    val bestScore: String,
+    @SerializedName("recordDeathsInAMatch")
+    val recordDeathsInMatch: String,
+    @SerializedName("recordKillsInAMatch")
+    val recordKillsInMatch: String,
+    @SerializedName("suicides")
+    val suicides: String,
+    @SerializedName("kills")
+    val totalKills: String,
+    @SerializedName("headshots")
+    val headshots: String,
+    @SerializedName("assists")
+    val assists: String,
+    @SerializedName("recordKillStreak")
+    val recordKillStreak: String
+) {}
