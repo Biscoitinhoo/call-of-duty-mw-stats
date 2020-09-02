52f80be266b3104f01997650a834cb36652dd3a7
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-09-02 08:51:20 -0300

    - UserInformationActivity.kt added;

---
+++ app/src/main/java/com/example/callofdutymw_stats/view/UserInformationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.callofdutymw_stats.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.callofdutymw_stats.R
+
+class UserInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_information)
+    }
+}
