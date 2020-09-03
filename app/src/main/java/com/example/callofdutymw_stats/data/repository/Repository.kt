06b0427431d046ffadbbec8d499ece9b4024d332
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-09-03 19:57:00 -0300

    - getWarzoneUser method added in repository and RepositoryImpl.kt;

--- app/src/main/java/com/example/callofdutymw_stats/data/repository/Repository.kt
+++ app/src/main/java/com/example/callofdutymw_stats/data/repository/Repository.kt
@@ -1,7 +1,9 @@
 package com.example.callofdutymw_stats.data.repository
 
 import com.example.callofdutymw_stats.model.multiplayer.lifetime.UserLifeTimeMultiplayer
+import com.example.callofdutymw_stats.model.warzone.dto.UserDtoWarzone
 
 interface Repository {
     suspend fun getMultiplayerUser(gamertag: String, platform: String): UserLifeTimeMultiplayer
+    suspend fun getWarzoneUser(gamertag: String, platform: String): UserDtoWarzone
 }
