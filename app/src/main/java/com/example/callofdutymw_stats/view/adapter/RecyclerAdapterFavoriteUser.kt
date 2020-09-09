20a081b6a43cf92f23a6e470371ed0b2e5a6a9d5
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-09-09 10:49:51 -0300

    - RecyclerAdapterFavoriteUser.kt configured; - recycler_view_favorite_user.xml layout added;

--- app/src/main/java/com/example/callofdutymw_stats/view/adapter/RecyclerAdapterFavoriteUser.kt
+++ app/src/main/java/com/example/callofdutymw_stats/view/adapter/RecyclerAdapterFavoriteUser.kt
@@ -1,5 +1,38 @@
 package com.example.callofdutymw_stats.view.adapter
 
-class RecyclerAdapterFavoriteUser {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.callofdutymw_stats.R
+import com.example.callofdutymw_stats.model.multiplayer.lifetime.all.properties.UserInformationMultiplayer
 
+class RecyclerAdapterFavoriteUser : RecyclerView.Adapter<RecyclerAdapterFavoriteUser.ViewHolder>() {
+
+    companion object {
+        val listOfFavoriteUser = ArrayList<UserInformationMultiplayer>()
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerAdapterFavoriteUser.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_view_favorite_user, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapterFavoriteUser.ViewHolder, position: Int) {
+        holder.bindItems(listOfFavoriteUser[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listOfFavoriteUser.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(userInformationMultiplayer: UserInformationMultiplayer) {
+
+        }
+    }
 }
