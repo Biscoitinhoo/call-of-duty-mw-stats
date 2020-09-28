7f992b47db1fb8147737b3e8ad7e51758457c0d3
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-09-28 19:43:37 -0300

    - all gun mode textViews declared in GamesFragment.kt;

--- app/src/main/java/com/example/callofdutymw_stats/fragment/GamesFragment.kt
+++ app/src/main/java/com/example/callofdutymw_stats/fragment/GamesFragment.kt
@@ -1,26 +1,69 @@
 package com.example.callofdutymw_stats.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.callofdutymw_stats.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GamesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GamesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
+
+    /** Gun mode */
+    private lateinit var textViewGunTotalKills: TextView
+    private lateinit var textViewGunKDRatio: TextView
+    private lateinit var imageViewGunKDArrow: ImageView
+    private lateinit var textViewGunTotalTimePlayed: TextView
+    private lateinit var textViewGunTotalDeaths: TextView
+    private lateinit var textViewGunScore: TextView
+
+    /** Domination */
+    private lateinit var textViewDomTotalKills: TextView
+    private lateinit var textViewDomKDRatio: TextView
+    private lateinit var imageViewDomKDArrow: ImageView
+    private lateinit var textViewDomTotalTimePlayed: TextView
+    private lateinit var textViewDomTotalDeaths: TextView
+    private lateinit var textViewDomScore: TextView
+
+    /** Arena */
+    private lateinit var textViewArenaTotalKills: TextView
+    private lateinit var textViewArenaKDRatio: TextView
+    private lateinit var imageViewArenaKDRatio: ImageView
+    private lateinit var textViewArenaTotalTimePlayed: TextView
+    private lateinit var textViewArenaTotalDeaths: TextView
+    private lateinit var textViewArenaScore: TextView
+
+    /** Infected */
+    private lateinit var textViewInfectedTotalKills: TextView
+    private lateinit var textViewInfectedKDRatio: TextView
+    private lateinit var imageViewInfectedKDArrow: ImageView
+    private lateinit var textViewInfectedTotalTimePlayed: TextView
+    private lateinit var textViewInfectedTotalDeaths: TextView
+    private lateinit var textViewInfectedScore: TextView
+
+    /** Headquarter */
+    private lateinit var textViewHQTotalKills: TextView
+    private lateinit var textViewHQKDRatio: TextView
+    private lateinit var imageViewHQKDArrow: ImageView
+    private lateinit var textViewHQTotalTimePlayed: TextView
+    private lateinit var textViewHQTotalDeaths: TextView
+    private lateinit var textViewHQScore: TextView
+
+    /** Ground war */
+    private lateinit var textViewGroundWarTotalKills: TextView
+    private lateinit var textViewGroundWarKDRatio: TextView
+    private lateinit var imageViewGroundWarKDArrow: ImageView
+    private lateinit var textViewGroundWarTotalTimePlayed: TextView
+    private lateinit var textViewGroundWarTotalDeaths: TextView
+    private lateinit var textViewGroundWarScore: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +77,10 @@ class GamesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_games, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GamesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             GamesFragment().apply {
