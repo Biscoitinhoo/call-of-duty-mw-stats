2a1ac990df6fea7f74f58d43d8fc8d4df8bed33c
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-09-28 18:44:30 -0300

    - all 'more' fragment user informations working;

--- app/src/main/java/com/example/callofdutymw_stats/fragment/MoreFragment.kt
+++ app/src/main/java/com/example/callofdutymw_stats/fragment/MoreFragment.kt
@@ -5,22 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.callofdutymw_stats.R
+import com.example.callofdutymw_stats.model.multiplayer.lifetime.all.properties.UserInformationMultiplayer
+import com.example.callofdutymw_stats.view.util.UserConstants
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MoreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MoreFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var textViewAccuracy: TextView
+    private lateinit var textViewHeadshots: TextView
+    private lateinit var textViewSuicides: TextView
+    private lateinit var textViewAssists: TextView
+    private lateinit var textViewRecordDeathsInMatch: TextView
+    private lateinit var textViewRecordWinStreak: TextView
+    private lateinit var textViewRecordXP: TextView
+    private lateinit var textViewTotalShots: TextView
+    private lateinit var textViewTotalShotsMisses: TextView
+    private lateinit var textViewTotalShotsHits: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +42,40 @@ class MoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        val view = inflater.inflate(R.layout.fragment_more, container, false)
+        setGeneralUserInformations(view)
+        return view
+    }
+
+    private fun setGeneralUserInformations(view: View) {
+        val userInformationMultiplayer = activity?.intent?.getSerializableExtra(UserConstants.OBJECT_USER) as UserInformationMultiplayer
+        findViews(view)
+        textViewAccuracy.text = userInformationMultiplayer.accuracy
+        textViewHeadshots.text = userInformationMultiplayer.headshots
+        textViewSuicides.text = userInformationMultiplayer.suicides
+        textViewAssists.text = userInformationMultiplayer.assists
+        textViewRecordDeathsInMatch.text = userInformationMultiplayer.recordDeathsInMatch
+        textViewRecordWinStreak.text = userInformationMultiplayer.recordWinStreak
+        textViewRecordXP.text = userInformationMultiplayer.recordXP
+        textViewTotalShots.text = userInformationMultiplayer.totalShots.toString()
+        textViewTotalShotsMisses.text = userInformationMultiplayer.totalShotsMisses.toString()
+        textViewTotalShotsHits.text = userInformationMultiplayer.totalShotsHits.toString()
+    }
+
+    private fun findViews(view: View) {
+        textViewAccuracy = view.findViewById(R.id.textViewAccuraccy) as TextView
+        textViewHeadshots = view.findViewById(R.id.textViewHeadshots) as TextView
+        textViewSuicides = view.findViewById(R.id.textViewSuicides) as TextView
+        textViewAssists = view.findViewById(R.id.textViewAssists) as TextView
+        textViewRecordDeathsInMatch = view.findViewById(R.id.textViewRecordDeathsInMatch) as TextView
+        textViewRecordWinStreak = view.findViewById(R.id.textViewRecordWins) as TextView
+        textViewRecordXP = view.findViewById(R.id.textViewTotalXP) as TextView
+        textViewTotalShots = view.findViewById(R.id.textViewTotalShots) as TextView
+        textViewTotalShotsMisses = view.findViewById(R.id.textViewTotalShotsMisses) as TextView
+        textViewTotalShotsHits = view.findViewById(R.id.textViewTotalShotsHits) as TextView
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MoreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             MoreFragment().apply {
